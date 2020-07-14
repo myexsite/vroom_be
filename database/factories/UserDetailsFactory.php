ac602afdfd3707da0d402b4a44def323704c297a
Ziad Hashim <exsite.in@gmail.com>
2020-07-15 00:21:26 +0530

    first commit

---
+++ database/factories/UserDetailsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\UserDetails;
+use Faker\Generator as Faker;
+
+$factory->define(UserDetails::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
